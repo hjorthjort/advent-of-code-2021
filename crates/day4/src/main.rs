7a166a0263d93aafbcad28760861f24bc4a62430
hjort <8545447+hjorthjort@users.noreply.github.com>
2021-12-04 22:14:23 +0100

    problem 1 day 4, find winning board

--- crates/day4/src/main.rs
+++ crates/day4/src/main.rs
@@ -1,4 +1,4 @@
-const INPUT : &str = include_str!("input.txt");
+const INPUT : &str = TEST_INPUT;// include_str!("input.txt");
 
 fn main() {
     println!("{}", solve_problem1(INPUT));
@@ -6,7 +6,81 @@ fn main() {
 }
 
 fn solve_problem1(input : &str) -> i32 {
+    let mut input: Vec<&str> = input.trim().lines().filter(|l| l != &"").collect();
+    let numbers: Vec<u8> = input.remove(0).split(",").map(|x| x.parse().unwrap()).collect();
+    let mut boards: Vec<[[u8;5];5]> = Vec::new();
+
+    for b in input.chunks(5) {
+        let board: [[u8;5];5] = (*b).iter().map(|l| line_to_nums(l)).collect::<Vec<[u8;5]>>().try_into().unwrap();
+        boards.push(board);
+    }
+    let mut drawn: Vec<u8> = Vec::new();
+    let mut winner = &[[0;5];5];
+    for i in 0..numbers.len() {
+        drawn.push(numbers[i]);
+        for b in boards.iter() {
+            if check_win(b, &drawn) {
+                winner = b;
+                break
+            }
+        }
+    }
+    println!("{:?}", winner);
     0
+}
+
+fn line_wins(line: &[u8;5], drawn: &Vec<u8>) -> bool {
+    for i in line {
+        if !(drawn.contains(&i)) {
+            return false; // this line is not a winner
+        }
+    }
+    true
+}
+
+#[test]
+fn test_line_wins() {
+    assert_eq!(line_wins(&[3,4,2,1,9],  &Vec::from([1,2,3,4,5,6,7,8,9])), true);
+    assert_eq!(line_wins(&[10,4,2,1,9], &Vec::from([1,2,3,4,5,6,7,8,9])), false);
+}
+
+fn transpose(mat: &[[u8; 5]; 5]) -> [[u8; 5]; 5] {
+    let mut out = [[0;5];5];
+    for i in 0..5 {
+        for j in 0..5 {
+            out[i][j] = mat[j][i];
+        }
+    }
+    out
+}
+
+#[test]
+fn test_transpose() {
+    assert_eq!(transpose(&[[1, 2, 3, 4, 5],[1, 2, 3, 4, 5],[1, 2, 3, 4, 5],[1, 2, 3, 4, 5],[1, 2, 3, 4, 5]]),
+                 [[1, 1, 1, 1, 1], [2, 2, 2, 2, 2], [3, 3, 3, 3, 3], [4, 4, 4, 4, 4],  [5, 5, 5, 5, 5]]);
+}
+
+fn check_win(board : &[[u8;5];5], drawn: &Vec<u8>) -> bool {
+    for l in board {
+        if line_wins(l, drawn) {
+            return true
+        }
+    }
+    for l in transpose(board) {
+        if line_wins(&l, drawn) {
+            return true
+        }
+    }
+    false
+}
+
+fn line_to_nums(line: &str) -> [u8;5] {
+    line.split_ascii_whitespace().take(5).map(|x| x.parse().unwrap()).collect::<Vec<u8>>().try_into().unwrap()
+}
+
+#[test]
+fn test_line_to_nums() {
+    assert_eq!(line_to_nums("32 23 0 19 3"), [32, 23, 0, 19, 3]);
 }
 
 fn solve_problem2(input : &str) -> i32 {
@@ -16,6 +90,25 @@ fn solve_problem2(input : &str) -> i32 {
 
 #[allow(dead_code)]
 const TEST_INPUT : &str = r#"
+7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7
 "#;
 
 #[test]
