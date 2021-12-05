61712fdb3fa2c0edbd5a2402aba147c9e08bb7e0
hjort <8545447+hjorthjort@users.noreply.github.com>
2021-12-05 12:25:44 +0100

    solve day4

--- crates/day4/src/main.rs
+++ crates/day4/src/main.rs
@@ -1,32 +1,37 @@
-const INPUT : &str = TEST_INPUT;// include_str!("input.txt");
+const INPUT : &str = include_str!("input.txt");
 
 fn main() {
     println!("{}", solve_problem1(INPUT));
     println!("{}", solve_problem2(INPUT));
 }
 
-fn solve_problem1(input : &str) -> i32 {
+fn solve_problem1(input : &str) -> u32 {
     let mut input: Vec<&str> = input.trim().lines().filter(|l| l != &"").collect();
     let numbers: Vec<u8> = input.remove(0).split(",").map(|x| x.parse().unwrap()).collect();
     let mut boards: Vec<[[u8;5];5]> = Vec::new();
-
     for b in input.chunks(5) {
         let board: [[u8;5];5] = (*b).iter().map(|l| line_to_nums(l)).collect::<Vec<[u8;5]>>().try_into().unwrap();
         boards.push(board);
     }
     let mut drawn: Vec<u8> = Vec::new();
-    let mut winner = &[[0;5];5];
     for i in 0..numbers.len() {
         drawn.push(numbers[i]);
         for b in boards.iter() {
             if check_win(b, &drawn) {
-                winner = b;
-                break
+                return
+                    present_winner(b, &drawn);
             }
         }
     }
-    println!("{:?}", winner);
     0
+}
+
+fn present_winner(board: &[[u8;5];5], drawn: &Vec<u8>) -> u32 {
+    println!("{:?}", board);
+    println!("{:?}", drawn);
+    let unmarked_sum : u32 = board.iter().flatten().filter(|x| !drawn.contains(*x)).map(|x| *x as u32).sum();
+    let last = *drawn.last().unwrap();
+    unmarked_sum * (last as u32)
 }
 
 fn line_wins(line: &[u8;5], drawn: &Vec<u8>) -> bool {
@@ -74,6 +79,13 @@ fn check_win(board : &[[u8;5];5], drawn: &Vec<u8>) -> bool {
     false
 }
 
+#[test]
+fn test_check_win() {
+    let board = [[14, 21, 17, 24, 4], [10, 16, 15, 9, 19], [18, 8, 23, 26, 20], [22, 11, 13, 6, 5], [2, 0, 12, 3, 7]];
+    let drawn = Vec::from([7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24]);
+    assert_eq!(check_win(&board, &drawn), true);
+}
+
 fn line_to_nums(line: &str) -> [u8;5] {
     line.split_ascii_whitespace().take(5).map(|x| x.parse().unwrap()).collect::<Vec<u8>>().try_into().unwrap()
 }
@@ -83,8 +95,30 @@ fn test_line_to_nums() {
     assert_eq!(line_to_nums("32 23 0 19 3"), [32, 23, 0, 19, 3]);
 }
 
-fn solve_problem2(input : &str) -> i32 {
-    0
+fn solve_problem2(input : &str) -> u32 {
+    let mut input: Vec<&str> = input.trim().lines().filter(|l| l != &"").collect();
+    let numbers: Vec<u8> = input.remove(0).split(",").map(|x| x.parse().unwrap()).collect();
+    let mut boards: Vec<[[u8;5];5]> = Vec::new();
+    for b in input.chunks(5) {
+        let board: [[u8;5];5] = (*b).iter().map(|l| line_to_nums(l)).collect::<Vec<[u8;5]>>().try_into().unwrap();
+        boards.push(board);
+    }
+    let mut drawn: Vec<u8> = Vec::new();
+    let mut winners: Vec<[[u8;5];5]> = Vec::new();
+    let mut win_draws : Vec<Vec<u8>> = Vec::new();
+    for i in 0..numbers.len() {
+        drawn.push(numbers[i]);
+        for b in boards.iter() {
+            if winners.contains(b) {
+                continue;
+            }
+            if check_win(b, &drawn) {
+                winners.push(*b);
+                win_draws.push(drawn.clone());
+            }
+        }
+    }
+    present_winner(winners.last().unwrap(), win_draws.last().unwrap())
 }
 
 
@@ -113,10 +147,10 @@ const TEST_INPUT : &str = r#"
 
 #[test]
 fn test_problem1() {
-    assert_eq!(solve_problem1(TEST_INPUT), 0);
+    assert_eq!(solve_problem1(TEST_INPUT), 4512);
 }
 
 #[test]
 fn test_problem2() {
-    assert_eq!(solve_problem1(TEST_INPUT), 0);
+    assert_eq!(solve_problem2(TEST_INPUT), 1924);
 }
